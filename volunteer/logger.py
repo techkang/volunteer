d0e4127ba855e13903a09f980d79a0eaca3f8aa9
kangs <ksc@mail.ustc.edu.cn>
2017-08-27 16:09:11 +0800

    change info while finished regesiter

---
+++ volunteer/logger.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+import os
+t = os.popen("logger.py")
+r = t.read()
+print r
--- volunteer/volunteer.py
+++ volunteer/volunteer.py
@@ -153,4 +153,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
