258351acf6db4fc87cdfb4e9c0a8f661ec32176a
kangs <ksc@mail.ustc.edu.cn>
2017-09-02 20:54:24 +0800

    port=80 do not work

--- volunteer/volunteer.py
+++ volunteer/volunteer.py
@@ -209,5 +209,5 @@ def download(filename='list.xls'):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80, debug=True)
+    app.run(host='0.0.0.0', debug=True)
     # app.run(debug=True)
